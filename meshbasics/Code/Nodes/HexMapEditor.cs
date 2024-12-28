71cccd3fb13631047244652f3fcd42378d15af60
JimmyHaglund <JimmyHaglund@Gmail.com>
2024-12-28 21:30:10 +0100

    ADD Hex Map 5.3.1: Optional Color and Elevation

--- meshbasics/Code/Nodes/HexMapEditor.cs
+++ meshbasics/Code/Nodes/HexMapEditor.cs
@@ -2,48 +2,74 @@
 
 namespace JHM.MeshBasics;
 
-public sealed partial class HexMapEditor : Node2D {
+public sealed partial class HexMapEditor : Control {
     [Export]
     public Color[] Colors { get; set; }
     [Export] public HexGrid HexGrid { get; set; }
 
     private Color _activeColor;
-    private int _activeElevation;
+    private int _activeElevation = 1;
     private bool _mouseIsDown = false;
+    private bool _applyColor = false;
+    private bool _applyElevation = true;
 
-    public override void _Ready() {
-        SelectColor(0);
-    }
+    // public override void _PhysicsProcess(double _) {
+    //     if (Input.IsMouseButtonPressed(MouseButton.Left)) {
+    //         if (_mouseIsDown) return;
+    //         HandleInput();
+    //     } else {
+    //         _mouseIsDown = false;
+    //     }
+    // }
 
-    public override void _PhysicsProcess(double _) {
+    public override void _UnhandledInput(InputEvent @event) {
         if (Input.IsMouseButtonPressed(MouseButton.Left)) {
             if (_mouseIsDown) return;
             HandleInput();
-        } else {
+        }
+        else {
             _mouseIsDown = false;
         }
     }
 
     void HandleInput() {
         if (HexGrid.IsRefreshing) return;
-
         var mousePosition = Mouse3D.MouseWorldPosition;
         EditCell(HexGrid.GetCell(mousePosition));
     }
 
     void EditCell(HexCell cell) {
         if (cell is null) return;
-        cell.Color = _activeColor;
-        cell.Elevation = _activeElevation;
+        if (_applyColor) {
+            cell.Color = _activeColor;
+        }
+        if (_applyElevation) {
+            cell.Elevation = _activeElevation;
+        }
         // HexGrid.Refresh();
     }
 
-    public void SetElevation(float elevationPercentage) {
-        var result = HexMetrics.Maxelevation * elevationPercentage / 100;
-        _activeElevation = (int)result;
+    public void SetElevation(int elevationStep) {
+        // var result = HexMetrics.Maxelevation * elevationStep / 100;
+        _activeElevation = elevationStep;
     }
 
     public void SelectColor(int index) {
+        if (index < 0 || index >= Colors.Length) {
+            _applyColor = false;
+            return;
+        }
         _activeColor = Colors[index];
+        _applyColor = true;
+    }
+
+    public void SetColor(Color color) {
+        _activeColor = color;
+    }
+
+    public void SetApplyColor(bool value) => _applyColor = value;
+
+    public void ToggleElevationEnabled(bool value) {
+        _applyElevation = value;
     }
 }
