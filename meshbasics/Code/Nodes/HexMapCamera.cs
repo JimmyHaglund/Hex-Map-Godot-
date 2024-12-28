7d7c9ab7d1dcef70269c010e0e9735d1dd12e51f
JimmyHaglund <JimmyHaglund@Gmail.com>
2024-12-28 19:19:12 +0100

    ADD Hex Map 5.2.0: Camera Controls

---
+++ meshbasics/Code/Nodes/HexMapCamera.cs
@@ -0,0 +1,13 @@
+﻿using Godot;
+
+namespace JHM.MeshBasics;
+
+public sealed partial class HexMapCamera : Node3D {
+    public Node3D Stick { get; set; }
+    public Node3D Swivel { get; set; }
+
+    public override void _EnterTree() {
+        Swivel = GetChild<Node3D>(0);
+        Stick = Swivel.GetChild<Node3D>(0);
+    }
+}
