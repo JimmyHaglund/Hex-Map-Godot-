b7465c36ea05485e627c466ffa653e589b520cd1
JimmyHaglund <JimmyHaglund@Gmail.com>
2024-12-28 07:05:23 +0100

    ADD Hex Map 3.2.0: Terraced Edge Connections

--- meshbasics/Code/HexMetrics.cs
+++ meshbasics/Code/HexMetrics.cs
@@ -8,9 +8,13 @@ public static class HexMetrics {
     public const float SolidFactor = 0.75f;
     public const float ElevationStep = 5.0f;
     public const float Maxelevation = 5.0f;
+    public const int TerracesPerSlope = 2;
 
     public const float InnerRadius = OuterRadius * 0.866025404f;
     public const float BlendFactor = 1.0f - SolidFactor;
+    public const int TerraceSteps = TerracesPerSlope * 2 + 1;
+    public const float HorizontalTerraceStepSize = 1.0f / TerraceSteps;
+    public const float VerticalTerraceStepSize = 1f / (TerracesPerSlope + 1);
 
     public static Vector3[] Corners = {
         new (0.0f, 0.0f, OuterRadius),
@@ -40,5 +44,19 @@ public static class HexMetrics {
 
     public static Vector3 GetBridge(HexDirection direction) {
         return (Corners[(int)direction] + Corners[(int)direction + 1]) * BlendFactor;
+    }
+
+    public static Vector3 TerraceLerp(Vector3 a, Vector3 b, int step) {
+        float h = step * HexMetrics.HorizontalTerraceStepSize;
+        a.X += (b.X - a.X) * h;
+        a.Z += (b.Z - a.Z) * h;
+        float v = ((step + 1) / 2) * HexMetrics.VerticalTerraceStepSize;
+        a.Y += (b.Y - a.Y) * v;
+        return a;
+    }
+
+    public static Color TerraceLerp(Color a, Color b, int step) {
+        float h = step * HexMetrics.HorizontalTerraceStepSize;
+        return a.Lerp(b, h);
     }
 }
