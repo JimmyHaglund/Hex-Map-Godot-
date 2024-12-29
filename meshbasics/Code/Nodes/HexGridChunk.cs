bf11868f0bb495fbf0e4adcea9f5573fe34fa72c
JimmyHaglund <JimmyHaglund@Gmail.com>
2024-12-29 03:40:06 +0100

    ADD 5.7.5: Stretching V Coordinates

--- meshbasics/Code/Nodes/HexGridChunk.cs
+++ meshbasics/Code/Nodes/HexGridChunk.cs
@@ -152,8 +152,8 @@ public sealed partial class HexGridChunk : Node3D {
         Terrain.AddTriangleColor(cell.Color);
 
         bool reversed = cell.IncomingRiver == direction;
-        TriangulateRiverQuad(centerL, centerR, m.v2, m.v4, cell.RiverSurfaceY, reversed);
-        TriangulateRiverQuad(m.v2, m.v4, e.v2, e.v4, cell.RiverSurfaceY, reversed);
+        TriangulateRiverQuad(centerL, centerR, m.v2, m.v4, cell.RiverSurfaceY, 0.4f, reversed);
+        TriangulateRiverQuad(m.v2, m.v4, e.v2, e.v4, cell.RiverSurfaceY, 0.6f, reversed);
     }
 
     private void TriangulateWithRiverBeginOrEnd(
@@ -169,19 +169,23 @@ public sealed partial class HexGridChunk : Node3D {
 
         bool reversed = cell.HasIncomingRiver;
         TriangulateRiverQuad(
-            m.v2, m.v4, e.v2, e.v4, cell.RiverSurfaceY, reversed
+            m.v2, m.v4, e.v2, e.v4, cell.RiverSurfaceY, 0.6f, reversed
         );
 
         center.Y = m.v2.Y = m.v4.Y = cell.RiverSurfaceY;
         Rivers.AddTriangle(center, m.v2, m.v4);
         if (reversed) {
             Rivers.AddTriangleUV(
-                new Vector2(0.5f, 1f), new Vector2(1f, 0f), new Vector2(0f, 0f)
+                new Vector2(0.5f, 0.4f),
+                new Vector2(1f, 0.2f),
+                new Vector2(0f, 0.2f)
             );
         }
         else {
             Rivers.AddTriangleUV(
-                new Vector2(0.5f, 0f), new Vector2(0f, 1f), new Vector2(1f, 1f)
+                new Vector2(0.5f, 0.4f),
+                new Vector2(0f, 0.6f),
+                new Vector2(1f, 0.6f)
             );
         }
     }
@@ -227,7 +231,7 @@ public sealed partial class HexGridChunk : Node3D {
             e2.v3.Y = neighbor.StreamBedY;
             TriangulateRiverQuad(
                 e1.v2, e1.v4, e2.v2, e2.v4,
-                cell.RiverSurfaceY, neighbor.RiverSurfaceY,
+                cell.RiverSurfaceY, neighbor.RiverSurfaceY, 0.8f,
                 cell.HasIncomingRiver && cell.IncomingRiver == direction
             );
         }
@@ -474,22 +478,22 @@ public sealed partial class HexGridChunk : Node3D {
 
     private void TriangulateRiverQuad(
         Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4,
-        float y1, float y2, bool reversed
+        float y1, float y2, float v, bool reversed
     ) {
         v1.Y = v2.Y = y1;
         v3.Y = v4.Y = y2;
         Rivers.AddQuad(v1, v2, v3, v4);
         if (reversed) {
-            Rivers.AddQuadUV(1.0f, 0.0f, 1.0f, 0.0f);
+            Rivers.AddQuadUV(1.0f, 0.0f, 0.8f - v, 0.6f - v);
         } else { 
-            Rivers.AddQuadUV(0f, 1f, 0f, 1f);
+            Rivers.AddQuadUV(0.0f, 1.0f, v, v + 0.2f);
         }
     }
 
     private void TriangulateRiverQuad(
         Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4,
-        float y, bool reversed
+        float y, float v, bool reversed
     ) {
-        TriangulateRiverQuad(v1, v2, v3, v4, y, y, reversed);
+        TriangulateRiverQuad(v1, v2, v3, v4, y, y, v, reversed);
     }
 }
