b6b6a7785f41f37a73c44ab5912f181f8bdcec25
JimmyHaglund <JimmyHaglund@Gmail.com>
2024-12-30 07:48:42 +0100

    ADD 10.3.1: Following the Edge

--- meshbasics/Code/Nodes/HexFeatureManager.cs
+++ meshbasics/Code/Nodes/HexFeatureManager.cs
@@ -70,7 +70,10 @@ public sealed partial class HexFeatureManager : Node3D {
         HexCell farCell
     ) {
         if (nearCell.Walled != farCell.Walled) {
-            AddWallSegment(near.v1, far.v1, near.v5, far.v5);
+            AddWallSegment(near.v1, far.v1, near.v2, far.v2);
+            AddWallSegment(near.v2, far.v2, near.v3, far.v3);
+            AddWallSegment(near.v3, far.v3, near.v4, far.v4);
+            AddWallSegment(near.v4, far.v4, near.v5, far.v5);
         }
     }
 
