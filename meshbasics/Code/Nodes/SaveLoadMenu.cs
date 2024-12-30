dc4767b7245b48a58ae1ad54a09abaa73b0e55da
JimmyHaglund <JimmyHaglund@Gmail.com>
2024-12-30 23:28:26 +0100

    ADD 13.3.2: Changing Appearance

--- meshbasics/Code/Nodes/SaveLoadMenu.cs
+++ meshbasics/Code/Nodes/SaveLoadMenu.cs
@@ -6,12 +6,21 @@ public sealed partial class SaveLoadMenu : Control {
     private bool _saveMode;
 
     [Export] public HexGrid HexGrid { get; set; }
+    [Export] public Label Title { get; set; }
+    [Export] public Button ActionButton { get; set; }
 
     public void Open(bool saveMode) {
         _saveMode = saveMode;
+        if (saveMode) {
+            Title.Text = "Save Map";
+            ActionButton.Text = "Save";
+        } else {
+            Title.Text = "Load Map";
+            ActionButton.Text = "Load";
+        }
+
         Visible = true;
         HexMapCamera.SetInstanceLocked(true);
-
     }
 
     public void Close() {
