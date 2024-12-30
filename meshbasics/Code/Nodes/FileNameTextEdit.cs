8b9588b8bafa9eef8df8d7a7642ddc8cd943d01f
JimmyHaglund <JimmyHaglund@Gmail.com>
2024-12-30 23:48:07 +0100

    ADD 13.3.3: Entering a Map Name

---
+++ meshbasics/Code/Nodes/FileNameTextEdit.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+using Godot;
+
+namespace JHM.MeshBasics;
+public sealed partial class FileNameTextEdit : TextEdit {
+    [Export] private string _allowedCharactersRegex = "^[a-zA-Z0-9_\\-\\. ]*$"; // Adjust the regex to allow your desired characters.
+
+    public override void _Ready() {
+        TextChanged += ValidateText;
+    }
+
+    private void ValidateText() {
+        var text = Text;
+        if (Regex.IsMatch(text, _allowedCharactersRegex)) return;
+
+        Text = Regex.Replace(text, "[^a-zA-Z0-9_\\-\\. ]", "");
+        // Reset the caret position to match user's editing flow.
+        var line = GetLineCount() - 1;
+        SetCaretLine(line);
+        SetCaretColumn(GetLine(line).Length);
+    }
+}
--- meshbasics/Code/Nodes/SaveLoadMenu.cs
+++ meshbasics/Code/Nodes/SaveLoadMenu.cs
@@ -1,4 +1,6 @@
-﻿using Godot;
+﻿using System.IO;
+using Godot;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace JHM.MeshBasics;
 
@@ -8,6 +10,7 @@ public sealed partial class SaveLoadMenu : Control {
     [Export] public HexGrid HexGrid { get; set; }
     [Export] public Label Title { get; set; }
     [Export] public Button ActionButton { get; set; }
+    [Export] public TextEdit NameInput { get; set; }
 
     public void Open(bool saveMode) {
         _saveMode = saveMode;
@@ -27,4 +30,14 @@ public sealed partial class SaveLoadMenu : Control {
         Visible = false;
         HexMapCamera.SetInstanceLocked(false);
     }
+
+    private string GetSelectedPath() {
+        string mapName = NameInput.Text;
+        if (mapName.Length == 0) {
+            return null;
+        }
+        return Path.Combine(GetFilePath(mapName + ".map"));
+    }
+
+    private string GetFilePath(string fileName) => Path.Combine(OS.GetUserDataDir(), fileName);
 }
