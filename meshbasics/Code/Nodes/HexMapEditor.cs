a1f794031a10b613e1ec9fe5e7c7d6e4c619d619
JimmyHaglund <JimmyHaglund@Gmail.com>
2024-12-31 10:04:36 +0100

    ADD 15.2.1: Edit Toggle

--- meshbasics/Code/Nodes/HexMapEditor.cs
+++ meshbasics/Code/Nodes/HexMapEditor.cs
@@ -9,7 +9,8 @@ public sealed partial class HexMapEditor : Control {
     [Export] private ShaderMaterial _terrainMaterial;
     [Export] public Color[] Colors { get; set; }
     [Export] public HexGrid HexGrid { get; set; }
-    
+
+    private bool _editMode = false;
     private int _activeElevation = 1;
     private int _activeTerrainTypeIndex = 0;
     private bool _applyElevation = false;
@@ -31,6 +32,7 @@ public sealed partial class HexMapEditor : Control {
     private int _activeFarmLevel = 1;
     private int _activePlantLevel = 1;
     private int _activeSpecialIndex = 1;
+    
 
     public override void _EnterTree() {
         base._EnterTree();
@@ -59,90 +61,14 @@ public sealed partial class HexMapEditor : Control {
         } else {
             _isDrag = false;
         }
-
-        EditCells(cell);
+        if (_editMode) { 
+            EditCells(cell);
+        }
         _previousCell = cell;
     }
 
-    private void ValidateDrag(HexCell currentCell) {
-        for (
-            _dragDirection = HexDirection.NE;
-            _dragDirection <= HexDirection.NW;
-            _dragDirection++
-        ) {
-            if (_previousCell.GetNeighbor(_dragDirection) == currentCell) {
-                _isDrag = true;
-                return;
-            }
-        }
-        _isDrag = false;
-    }
-
-    private void EditCells(HexCell center) {
-        if (center is null) return;
-        int centerX = center.Coordinates.X;
-        int centerZ = center.Coordinates.Z;
-        for (int r = 0, z = centerZ - _brushSize; z <= centerZ; z++, r++) {
-            for (int x = centerX - r; x <= centerX + _brushSize; x++) {
-                EditCell(HexGrid.GetCell(new HexCoordinates(x, z)));
-            }
-        }
-
-        for (int r = 0, z = centerZ + _brushSize; z > centerZ; z--, r++) {
-            for (int x = centerX - _brushSize; x <= centerX + r; x++) {
-                EditCell(HexGrid.GetCell(new HexCoordinates(x, z)));
-            }
-        }
-
-    }
-
-    private void EditCell(HexCell cell) {
-        if (cell is null) return;
-
-        if (_applyTerrainType) {
-            cell.TerrainTypeIndex = _activeTerrainTypeIndex;
-        }
-
-        if (_applyElevation) {
-            cell.Elevation = _activeElevation;
-        }
-        if (_applyWaterLevel) {
-            cell.WaterLevel = _activeWaterLevel;
-        }
-
-        if (_riverMode == OptionalToggle.Off) {
-            cell.RemoveRiver();
-        }
-        if (_roadMode == OptionalToggle.Off) {
-            cell.RemoveRoads();
-        }
-        if (_wallMode != OptionalToggle.Disable) {
-            cell.Walled = _wallMode == OptionalToggle.On;
-        }
-
-        if (_isDrag) {
-            HexCell otherCell = cell.GetNeighbor(_dragDirection.Opposite());
-            if (otherCell is not null) { 
-                if (_riverMode == OptionalToggle.On) {
-                    otherCell.SetOutgoingRiver(_dragDirection);
-                }
-                if (_roadMode == OptionalToggle.On) { 
-                    otherCell.AddRoad(_dragDirection);
-                }
-            }
-        }
-        if (_applySpecialIndex) {
-            cell.SpecialIndex = _activeSpecialIndex;
-        }
-        if (_applyUrbanLevel) {
-            cell.UrbanLevel = _activeUrbanLevel;
-        }
-        if (_applyFarmLevel) {
-            cell.FarmLevel = _activeFarmLevel;
-        }
-        if (_applyPlantLevel) {
-            cell.PlantLevel = _activePlantLevel;
-        }
+    public void SetEditMode(bool value) {
+        _editMode = value;
     }
 
     public void SetElevation(float elevationStep) {
@@ -153,6 +79,7 @@ public sealed partial class HexMapEditor : Control {
     public void SetTerrainTypeIndex(float index) {
         _activeTerrainTypeIndex = (int)index;
     }
+
     public void ToggleElevationEnabled(bool value) {
         _applyElevation = value;
     }
@@ -218,6 +145,87 @@ public sealed partial class HexMapEditor : Control {
     public void ShowGrid(bool visible) {
         _terrainMaterial.SetShaderParameter("GRID_ON", visible);
         GD.Print(_terrainMaterial.Get("GRID_ON").AsBool());
+    }
+
+    private void ValidateDrag(HexCell currentCell) {
+        for (
+            _dragDirection = HexDirection.NE;
+            _dragDirection <= HexDirection.NW;
+            _dragDirection++
+        ) {
+            if (_previousCell.GetNeighbor(_dragDirection) == currentCell) {
+                _isDrag = true;
+                return;
+            }
+        }
+        _isDrag = false;
+    }
+
+    private void EditCells(HexCell center) {
+        if (center is null) return;
+        int centerX = center.Coordinates.X;
+        int centerZ = center.Coordinates.Z;
+        for (int r = 0, z = centerZ - _brushSize; z <= centerZ; z++, r++) {
+            for (int x = centerX - r; x <= centerX + _brushSize; x++) {
+                EditCell(HexGrid.GetCell(new HexCoordinates(x, z)));
+            }
+        }
+
+        for (int r = 0, z = centerZ + _brushSize; z > centerZ; z--, r++) {
+            for (int x = centerX - _brushSize; x <= centerX + r; x++) {
+                EditCell(HexGrid.GetCell(new HexCoordinates(x, z)));
+            }
+        }
+
+    }
+
+    private void EditCell(HexCell cell) {
+        if (cell is null) return;
+
+        if (_applyTerrainType) {
+            cell.TerrainTypeIndex = _activeTerrainTypeIndex;
+        }
+
+        if (_applyElevation) {
+            cell.Elevation = _activeElevation;
+        }
+        if (_applyWaterLevel) {
+            cell.WaterLevel = _activeWaterLevel;
+        }
+
+        if (_riverMode == OptionalToggle.Off) {
+            cell.RemoveRiver();
+        }
+        if (_roadMode == OptionalToggle.Off) {
+            cell.RemoveRoads();
+        }
+        if (_wallMode != OptionalToggle.Disable) {
+            cell.Walled = _wallMode == OptionalToggle.On;
+        }
+
+        if (_isDrag) {
+            HexCell otherCell = cell.GetNeighbor(_dragDirection.Opposite());
+            if (otherCell is not null) {
+                if (_riverMode == OptionalToggle.On) {
+                    otherCell.SetOutgoingRiver(_dragDirection);
+                }
+                if (_roadMode == OptionalToggle.On) {
+                    otherCell.AddRoad(_dragDirection);
+                }
+            }
+        }
+        if (_applySpecialIndex) {
+            cell.SpecialIndex = _activeSpecialIndex;
+        }
+        if (_applyUrbanLevel) {
+            cell.UrbanLevel = _activeUrbanLevel;
+        }
+        if (_applyFarmLevel) {
+            cell.FarmLevel = _activeFarmLevel;
+        }
+        if (_applyPlantLevel) {
+            cell.PlantLevel = _activePlantLevel;
+        }
     }
 
     #region Definitions
