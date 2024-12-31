2c1a56bf1dab2345f18ff6404ebbf8ca48cd679e
JimmyHaglund <JimmyHaglund@Gmail.com>
2025-01-01 00:58:47 +0100

    ADD 18.3.4: Selecting a Unit

--- meshbasics/Code/Nodes/HexGameUI.cs
+++ meshbasics/Code/Nodes/HexGameUI.cs
@@ -4,6 +4,7 @@ namespace JHM.MeshBasics;
 
 public sealed partial class HexGameUI : Control {
     private HexCell _currentCell;
+    private HexUnit _selectedUnit;
 
     [Export] public HexGrid Grid { get; set; }
     
@@ -13,10 +14,24 @@ public sealed partial class HexGameUI : Control {
         Grid.SetUIVisible(!toggle);
     }
 
+    public override void _UnhandledInput(InputEvent @event) {
+        if (Input.IsMouseButtonPressed(MouseButton.Left)) { 
+            DoSelection();
+        }
+    }
+
+
     private bool UpdateCurrentCell() {
         var cell = Grid.GetCell(Mouse3D.MouseWorldPosition);
         if (cell == _currentCell) return false;
         _currentCell = cell;
         return true;
+    }
+
+    private void DoSelection() {
+        UpdateCurrentCell();
+        if (_currentCell is not null) {
+            _selectedUnit = _currentCell.Unit;
+        }
     }
 }
