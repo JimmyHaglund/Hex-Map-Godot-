12cb0a1e02f617cd174c7aed3efd48dcfde489c5
JimmyHaglund <JimmyHaglund@Gmail.com>
2024-12-31 03:19:29 +0100

    ADD 14.2.4: Textures

---
+++ meshbasics/Code/Nodes/Texture2DArrayWizard.cs
@@ -0,0 +1,18 @@
+﻿using Godot;
+
+namespace JHM.MeshBasics;
+
+public sealed partial class Texture2DArrayWizard : Node {
+    [Export] public Texture2D[] Images { get; set; }
+
+    public override void _Ready() {
+        var textureArray = new Texture2DArray();
+        var images = new Image[Images.Length];
+        for (var n = 0; n < images.Length; n++) {
+            images[n] = Images[n].GetImage();
+        }
+        textureArray.CreateFromImages(new(images));
+
+        ResourceSaver.Save(textureArray, "res://misc/TerrainTexture2DArray.tres");
+    }
+}
