c39e8fcab46573f186bc33b5acd25bb9b9c3b589
JimmyHaglund <JimmyHaglund@Gmail.com>
2025-01-01 00:56:19 +0100

    ADD 18.3.3: Keeping Track of the Current Cell

--- meshbasics/Code/Nodes/HexGameUI.cs
+++ meshbasics/Code/Nodes/HexGameUI.cs
@@ -3,11 +3,20 @@
 namespace JHM.MeshBasics;
 
 public sealed partial class HexGameUI : Control {
-    [Export] public HexGrid Grid {get; set; }
+    private HexCell _currentCell;
 
+    [Export] public HexGrid Grid { get; set; }
+    
     public void SetEditMode(bool toggle) { 
         Visible = !toggle;
         ProcessMode = toggle ? ProcessModeEnum.Disabled : ProcessModeEnum.Inherit;
         Grid.SetUIVisible(!toggle);
+    }
+
+    private bool UpdateCurrentCell() {
+        var cell = Grid.GetCell(Mouse3D.MouseWorldPosition);
+        if (cell == _currentCell) return false;
+        _currentCell = cell;
+        return true;
     }
 }
