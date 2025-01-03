424a3dafebd71a2a62fa3b9a30b366f4eeb5dc47
JimmyHaglund <JimmyHaglund@Gmail.com>
2025-01-03 08:53:18 +0100

    ADD 23.1.4: Accessing the Cells

--- meshbasics/Code/Nodes/HexMapGenerator.cs
+++ meshbasics/Code/Nodes/HexMapGenerator.cs
@@ -7,5 +7,8 @@ public sealed partial class HexMapGenerator : Node {
 
     public void GenerateMap(int x, int z) {
         Grid.CreateMap(x, z);
+        for (int i = 0; i < z; i++) {
+            Grid.GetCell(x / 2, i).TerrainTypeIndex = 1;
+        }
     }
 }
