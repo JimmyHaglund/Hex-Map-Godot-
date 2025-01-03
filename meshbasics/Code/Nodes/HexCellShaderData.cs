ee7ac59aac7b1645dc3174cf4a023de5d091e142
JimmyHaglund <JimmyHaglund@Gmail.com>
2025-01-03 07:13:11 +0100

    ADD 22.1.3: Updating Cell Data

--- meshbasics/Code/Nodes/HexCellShaderData.cs
+++ meshbasics/Code/Nodes/HexCellShaderData.cs
@@ -1,13 +1,17 @@
 ﻿using Godot;
+using System.Collections.Generic;
 namespace JHM.MeshBasics;
 
 public sealed partial class HexCellShaderData : Node {
     [Export] private ShaderMaterial[] _shaders;
 
+    private const float _transitionSpeed = 1.0f;
     private ImageTexture _cellTexture;
     private Image _image;
     private Color[] _cellTextureData;
     private static HexCellShaderData _instance;
+    private List<HexCell> _transitioningCells = new();
+
     public bool ImmediateMode { get; set; } = false;
 
     public static void SetShaderParameter(string parameterName, Godot.Variant value) { 
@@ -16,14 +20,6 @@ public sealed partial class HexCellShaderData : Node {
             shader.SetShaderParameter(parameterName, value);
         }
     } 
-
-    public override void _EnterTree() {
-        _instance = this;
-    }
-
-    public override void _ExitTree() {
-        if (_instance == this) _instance = null;
-    }
 
     public void Initialize(int x, int z) {
         //if (_cellTexture is not null) {
@@ -58,6 +54,7 @@ public sealed partial class HexCellShaderData : Node {
                 _cellTextureData[i] = new Color(0, 0, 0, 0);
             }
         }
+        _transitioningCells.Clear();
         ProcessMode = ProcessModeEnum.Inherit;
     }
 
@@ -69,25 +66,76 @@ public sealed partial class HexCellShaderData : Node {
 
     public void RefreshVisibility(HexCell cell) {
         int index = cell.Index;
-        _cellTextureData[index].R = cell.IsVisible ? 1.0f : 0.0f;
-        _cellTextureData[index].G = cell.IsExplored ? 1.0f : 0.0f;
+        if (ImmediateMode) {
+            _cellTextureData[index].R = cell.IsVisible ? 1.0f : 0.0f;
+            _cellTextureData[index].G = cell.IsExplored ? 1.0f : 0.0f;
+        } else { 
+            _transitioningCells.Add(cell);
+        }
         ProcessMode = ProcessModeEnum.Inherit;
     }
 
-    public override void _Process(double delta) {
-        CallDeferred("LateUpdate");
+    public override void _EnterTree() {
+        _instance = this;
     }
 
-    private void LateUpdate() {
+    public override void _ExitTree() {
+        if (_instance == this) _instance = null;
+    }
+
+    public override void _Process(double delta) {
+        CallDeferred("LateUpdate", (float)delta);
+    }
+
+    private bool UpdateCellData(HexCell cell, float delta) {
+        int index = cell.Index;
+        Color data = _cellTextureData[index];
+        bool stillUpdating = false;
+
+        if (cell.IsExplored && data.G < 1.0f) {
+            stillUpdating = true;
+            float t = data.G + delta;
+            data.G = Mathf.Min(t, 1.0f);
+        }
+
+        if (cell.IsVisible) {
+            if (data.R < 1.0f) {
+                stillUpdating = true;
+                float t = data.R + delta;
+                data.R = Mathf.Min(t, 1.0f);
+            }
+        }
+        else if (data.R > 0.0f) {
+            stillUpdating = true;
+            float t = data.R - delta;
+            data.R = t < 0.0f ? 0.0f : t;
+        }
+
+        _cellTextureData[index] = data;
+        return !stillUpdating;
+    }
+
+    private void LateUpdate(float deltaTime) {
+        var transitionDelta = deltaTime * _transitionSpeed;
+        for (int i = 0; i < _transitioningCells.Count; i++) {
+            var isFinished = UpdateCellData(_transitioningCells[i], transitionDelta);
+            if (isFinished) {
+                _transitioningCells[i--] = _transitioningCells[^1];
+                _transitioningCells.RemoveAt(_transitioningCells.Count - 1);
+            }
+        }
+
         var w = _cellTexture.GetWidth();
-        for (var x = 0; x < w; x++) { 
-            for(var y = 0; y < _cellTexture.GetHeight(); y++) { 
+        for (var x = 0; x < w; x++) {
+            for (var y = 0; y < _cellTexture.GetHeight(); y++) {
                 var c = _cellTextureData[x + w * y];
                 _image.SetPixel(x, y, c);
             }
         }
         _cellTexture.Update(_image);
 
-        this.ProcessMode = ProcessModeEnum.Disabled;
+        if (_transitioningCells.Count == 0) { 
+            ProcessMode = ProcessModeEnum.Disabled;
+        }
     }
 }
