76ccda61c10a2536ecbdef06c9186259d329d358
JimmyHaglund <JimmyHaglund@Gmail.com>
2025-01-03 08:50:03 +0100

    ADD 23.1.3: Adjusting the New Map Menu

---
+++ meshbasics/Code/Nodes/CreateMapMenu.cs
@@ -0,0 +1,20 @@
+﻿using Godot;
+
+namespace JHM.MeshBasics;
+
+public sealed partial class CreateMapMenu : Control {
+    [Export] private HexGrid _grid;
+    [Export] private HexMapGenerator _generator;
+
+    private bool _generate;
+
+    public void SetGenerate(bool value) => _generate = value;
+
+    public void CreateMap(int x, int z) { 
+        if (_generate) { 
+            _generator.GenerateMap(x, z);
+        } else { 
+            _grid.CreateMap(x, z);
+        }
+    }
+}
