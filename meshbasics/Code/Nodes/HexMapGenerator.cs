8b8d67e08c3e3ed4cc80617c2b811ceea97b487a
JimmyHaglund <JimmyHaglund@Gmail.com>
2025-01-03 08:35:19 +0100

    ADD 23.1.2: Map Generator

---
+++ meshbasics/Code/Nodes/HexMapGenerator.cs
@@ -0,0 +1,11 @@
+﻿using Godot;
+
+namespace JHM.MeshBasics;
+
+public sealed partial class HexMapGenerator : Node {
+    [Export]public HexGrid Grid {get; set; }
+
+    public void GenerateMap(int x, int z) {
+        Grid.CreateMap(x, z);
+    }
+}
