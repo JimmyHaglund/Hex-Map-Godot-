94e7db62ffa5c50c48759b8fb7010b16f461b1d2
JimmyHaglund <JimmyHaglund@Gmail.com>
2025-01-12 17:48:39 +0100

    REF HexUnit

---
+++ HexaGrid/HexUnit.cs
@@ -0,0 +1,174 @@
+﻿using Godot;
+
+namespace JHM.HexaGrid;
+
+public sealed class HexUnit {
+    private HexCell? _location;
+    private float _orientation;
+    private float _moveProgress = 0.0f;
+    private int _travelIndex = 0;
+    private float _travelSpeed = 4.0f;
+
+    public float TravelSpeed => _travelSpeed;
+    public static PackedScene UnitPrefab { get; set; }
+    public HexGrid? Grid { get; set; }
+
+    public int VisionRange => 3;
+    public List<HexCell>? PathToTravel {get; private set; }
+    public Vector3 Position { get; private set; }
+
+    public static Action<BinaryReader, HexGrid> LoadImplementation = (r, g) => { 
+        throw new NotImplementedException("Loading and instantiating units must be handled by engine-side code.");
+    };
+
+    private HexCell? CurrentTravelLocation {
+        get {
+            if (PathToTravel is null) return _location;
+            if (_travelIndex < 0 || _travelIndex >= PathToTravel.Count) return null;
+            return PathToTravel[_travelIndex];
+        }
+    }
+
+
+    public HexCell? Location {
+        get {
+            return _location;
+        }
+        set {
+            if (_location is not null) {
+                Grid?.DecreaseVisibility(_location, VisionRange);
+                _location.Unit = null;
+            }
+            if (value is null) return;
+            _location = value;
+            _location.Unit = this;
+            Grid?.IncreaseVisibility(_location, VisionRange);
+        }
+    }
+
+    public float Orientation {
+        get {
+            return _orientation;
+        }
+        set {
+            _orientation = value;
+        }
+    }
+
+    public int Speed => 24;
+
+
+    public static void Load(BinaryReader reader, HexGrid grid) {
+        LoadImplementation.Invoke(reader, grid);
+    }
+
+    public bool IsValidDestination(HexCell cell) {
+        if (cell is null) return false;
+        return cell.IsExplored && !cell.IsUnderwater && cell.Unit is null;
+    }
+
+    public int GetMoveCost(
+        HexCell fromCell,
+        HexCell toCell,
+        HexDirection direction
+    ) {
+        HexEdgeType edgeType = fromCell.GetEdgeType(toCell);
+        if (edgeType == HexEdgeType.Cliff) {
+            return -1;
+        }
+        int moveCost;
+        if (fromCell.HasRoadThroughEdge(direction)) {
+            moveCost = 1;
+        }
+        else if (fromCell.Walled != toCell.Walled) {
+            return -1;
+        }
+        else {
+            moveCost = edgeType == HexEdgeType.Flat ? 5 : 10;
+            moveCost += toCell.UrbanLevel + toCell.FarmLevel + toCell.PlantLevel;
+        }
+        return moveCost;
+    }
+
+    public void Travel(List<HexCell> path) {
+        Grid.DecreaseVisibility(CurrentTravelLocation, VisionRange);
+        if (_location is not null) {
+            _location.Unit = null;
+        }
+        _location = path[path.Count - 1];
+        _location.Unit = this;
+
+        PathToTravel = path;
+        _travelIndex = 0;
+        _moveProgress = 0.0f;
+        Grid.IncreaseVisibility(CurrentTravelLocation, VisionRange);
+    }
+
+    public void Die() {
+        Location = null;
+    }
+
+    public void Save(BinaryWriter writer) {
+        var coordinates = Location?.Coordinates ?? new HexCoordinates();
+        coordinates.Save(writer);
+        writer.Write(Orientation);
+    }
+
+    public void TravelPath(float delta) {
+        if (PathToTravel is null) return;
+        _moveProgress += delta * _travelSpeed;
+        int fromColumn = 0;
+        int toColumn = 0;
+        while (_moveProgress >= 1.0f) {
+            if (_travelIndex + 1 < PathToTravel.Count) {
+                Grid.DecreaseVisibility(CurrentTravelLocation, VisionRange);
+            }
+            fromColumn = toColumn = CurrentTravelLocation.ColumnIndex;
+            _travelIndex++;
+            _moveProgress--;
+            if (CurrentTravelLocation != null) {
+                toColumn = CurrentTravelLocation.ColumnIndex;
+                Grid.MakeChildOfColumn(this, CurrentTravelLocation.ColumnIndex);
+            }
+            if (_travelIndex > 0) {
+                Grid.IncreaseVisibility(CurrentTravelLocation, VisionRange);
+            }
+            if (_travelIndex + 1 >= PathToTravel.Count) break;
+        }
+        if (_travelIndex >= PathToTravel.Count) {
+            PathToTravel = null;
+            return;
+        }
+
+        Vector3 a, b, c;
+        a = PathToTravel[0].Position;
+        b = PathToTravel[_travelIndex].Position;
+        c = b;
+
+        if (_travelIndex > 0) {
+            a = (PathToTravel[_travelIndex - 1].Position + PathToTravel[_travelIndex].Position) * 0.5f;
+        }
+        if (_travelIndex + 1 < PathToTravel.Count) {
+            c = (b + PathToTravel[_travelIndex + 1].Position) * 0.5f;
+        }
+
+        if (toColumn < fromColumn - 1) {
+            a.X -= HexMetrics.InnerDiameter * HexMetrics.wrapSize;
+            b.X -= HexMetrics.InnerDiameter * HexMetrics.wrapSize;
+        }
+        else if (toColumn > fromColumn + 1) {
+            a.X += HexMetrics.InnerDiameter * HexMetrics.wrapSize;
+            b.X += HexMetrics.InnerDiameter * HexMetrics.wrapSize;
+        }
+        if (_travelIndex + 1 < PathToTravel.Count) {
+            c = (b + PathToTravel[_travelIndex + 1].Position) * 0.5f;
+        }
+
+        Position = Bezier.GetPoint(a, b, c, _moveProgress);
+    }
+
+    public void Dispose() { 
+        if (_location is null) return;
+        _location.Unit = null;
+    }
+}
---
+++ meshbasics/_Nodes/HexGrid/HexUnitNode.cs
@@ -0,0 +1,138 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using Godot;
+using JHM.HexaGrid;
+using static Godot.TextServer;
+
+namespace JHM.MeshBasics;
+
+public sealed partial class HexUnitNode : Node3D {
+    [Export] private PackedScene _pathDisplayPrefab;
+    private bool _hasDrawnPath = false;
+    private List<Node3D> _pathDisplays = new();
+
+    public HexUnit Unit { get; } = new();
+    public int VisionRange => Unit.VisionRange;
+    public HexGrid Grid { 
+        get => Unit.Grid;
+        set => Unit.Grid = value;
+    }
+    public HexCell Location {
+        get => Unit.Location;
+        set { 
+            Unit.Location = value;
+            if (value is null) return;
+            Position = value.Position;
+        }
+    }
+    public float Orientation {
+        get => Unit.Orientation;
+        set { 
+            Unit.Orientation = value;
+            Rotation = new(0.0f, value, 0.0f);
+        }
+    }
+    public List<HexCell> PathToTravel => Unit.PathToTravel;
+    public int Speed => Unit.Speed;
+    private float TravelSpeed => Unit.TravelSpeed;
+
+    public void ValidateLocation() {
+        Position = Unit.Position;
+    }
+
+    public bool IsValidDestination(HexCell cell) => Unit.IsValidDestination(cell);
+
+    public int GetMoveCost(HexCell fromCell, HexCell toCell, HexDirection direction) {
+        return Unit.GetMoveCost(fromCell, toCell, direction);
+    }
+
+    public void Travel(List<HexCell> path) {
+        Unit.Travel(path);
+        ClearPathDisplay();
+    }
+
+    public void Die() {
+        Unit.Die();
+        QueueFree();
+    }
+
+    public void Save(BinaryWriter writer) {
+        Unit.Save(writer);
+    }
+
+    public static void Load(BinaryReader reader, HexGridNode grid) {
+        HexUnit.LoadImplementation = LoadImplementation;
+        HexUnit.Load(reader, grid.Grid);
+    }
+
+    private static void LoadImplementation(BinaryReader reader, HexGrid grid) {
+        HexCoordinates coordinates = HexCoordinates.Load(reader);
+        float orientation = reader.ReadSingle();
+
+        var unit = SceneInstantiator.InstantiateOrphan<HexUnitNode>(HexUnit.UnitPrefab);
+        
+        grid.AddUnit(
+            unit.Unit,
+            grid.GetCell(coordinates),
+            orientation
+        );
+        grid.HexGridRoot.AddChild(unit);
+    }
+    
+    public override void _Process(double delta) {
+        var fDelta = (float)delta;
+        Unit.TravelPath(fDelta);
+        Position = Unit.Position;
+        DrawPath();
+    }
+
+    private void DrawPath() {
+        if (Unit.PathToTravel == null || PathToTravel.Count == 0 || _hasDrawnPath) {
+            return;
+        }
+        ClearPathDisplay();
+
+        Vector3 a, b, c = PathToTravel[0].Position;
+
+        for (int i = 1; i < PathToTravel.Count; i++) {
+            a = c;
+            b = PathToTravel[i - 1].Position;
+            c = (b + PathToTravel[i].Position) * 0.5f;
+            for (float t = 0f; t < 1f; t += 0.05f * Unit.TravelSpeed) {
+                var node = GetTree().Root.GetChild(0).InstantiateChild<Node3D>(_pathDisplayPrefab);
+                var pos = Bezier.GetPoint(a, b, c, t);
+                _pathDisplays.Add(node);
+                node.GlobalPosition = pos;
+                var derivative = Bezier.GetDerivative(a, b, c, t);
+                if (!derivative.IsEqualApprox(Vector3.Zero)) {
+                    node.LookAt(node.Position + derivative);
+                }
+            }
+        }
+
+        a = c;
+        b = PathToTravel[PathToTravel.Count - 1].Position;
+        c = b;
+        for (float t = 0f; t < 1f; t += 0.1f) {
+            var node = GetTree().Root.GetChild(0).InstantiateChild<Node3D>(_pathDisplayPrefab);
+            var pos = Bezier.GetPoint(a, b, c, t);
+            _pathDisplays.Add(node);
+            node.GlobalPosition = pos;
+            node.LookAt(node.Position + Bezier.GetDerivative(a,b,c,t));
+        }
+        _hasDrawnPath = true;
+    }
+
+    private void ClearPathDisplay() {
+        foreach (var n in _pathDisplays) n.QueueFree();
+        _pathDisplays.Clear();
+        _hasDrawnPath = false;
+    }
+
+    private bool _lookBeforeMoving = false;
+
+    public override void _ExitTree() {
+        ClearPathDisplay();
+        Unit.Dispose();
+    }
+}
